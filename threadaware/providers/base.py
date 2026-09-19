c1222915bba5b64e941aa972e987cabccb029b0d
FreezeGrover Labs <shahrukhyezdani33@gmail.com>
2026-09-19 19:21:24 +0500

    Allow provider calls to identify evaluation roles

--- threadaware/providers/base.py
+++ threadaware/providers/base.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from abc import ABC, abstractmethod
+
 from threadaware.common.models import Turn
 
 
@@ -12,5 +13,6 @@ class ModelProvider(ABC):
         model: str,
         messages: list[Turn],
         max_output_tokens: int = 1500,
+        role: str = "unspecified",
     ) -> str:
         raise NotImplementedError
